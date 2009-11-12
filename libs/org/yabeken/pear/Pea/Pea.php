922e23d9ddbebfb4911a25cd6fe1eb5f72c91424
Kentaro YABE <yabeken@gmail.com>
2009-11-12 13:42:47 +0900

    version 1.0 の依存関係はOKかな？

--- libs/org/yabeken/pear/Pea/Pea.php
+++ libs/org/yabeken/pear/Pea/Pea.php
@@ -10,10 +10,12 @@ class Pea{
 	static private $PEAR_PATH;
 	static private $IMPORTED = array();
 	static private $CHANNEL = array();
+	static private $INSTALL = array();
 	static private $PREFFERED_STATE = 0;
+	static private $OPTIONAL = false;
 	static private $states = array("stable"=>0,"beta"=>1,"alpha"=>2,"devel"=>3);
 	static private $prepared = false;
-	static private $INSTALL = array();
+	
 	
 	/**
 	 * PEARの準備準備
@@ -46,7 +48,7 @@ class Pea{
 	static public function import($package_path){
 		self::r();
 		list($domain,$package_name,$package_version) = self::parse_package($package_path);
-		if(isset(self::$IMPORTED[$domain."/".$package_name])) return;
+		if(isset(self::$IMPORTED[strtolower($domain."/".$package_name)])) return self::$IMPORTED[strtolower($domain."/".$package_name)];
 		self::install($package_path);
 	}
 	
@@ -55,17 +57,17 @@ class Pea{
 		list($name,$version) = (strpos($name,"-")===false) ? array($name,null) : explode("-",$name,2);
 		return array($domain,$name,$version);
 	}
-	
 	/**
 	 * インストール
 	 * @param string $package_path
 	 * @return boolean
 	 */
-	static public function install($package_path){
+	static public function install($package_path,$force=false){
 		list($domain,$package_name,$package_version) = self::parse_package($package_path);
+		if(isset(self::$INSTALL[strtolower($domain."/".$package_name)])) return;
 		if(!isset(self::$CHANNEL[$domain])) self::channel_discover($domain);
 		while(true){
-			$allreleases_xml = self::$CHANNEL[$domain]."/r/".$package_name."/allreleases.xml";
+			$allreleases_xml = self::$CHANNEL[$domain]."/r/".strtolower($package_name)."/allreleases.xml";
 			if(!Tag::setof($a,R(Http)->do_get($allreleases_xml)->body(),"a")) throw new RuntimeException($package_path." not found");
 			$target_package = $a->f("p.value()");
 			$target_version = null;
@@ -76,15 +78,17 @@ class Pea{
 			}
 			foreach($a->in("r") as $r){
 				$v = $r->f("v.value()");
-				if($package_version==$v){
+				if(!empty($package_version)){
+					if($package_version == $v) $target_version = $v;
+				}else if($force){
 					$target_version = $v;
-					break;
+				}else{
+					$s = $r->f("s.value()");
+					if(isset(self::$states[$s]) && self::$states[$s] <= $target_state){
+						$target_version = $v;
+					}
 				}
-				$s = $r->f("s.value()");
-				if(isset(self::$states[$s]) && self::$states[$s] <= $target_state){
-					$target_version = $v;
-					break;
-				}
+				if(!empty($target_version)) break;
 			}
 			if(empty($target_version)) throw new RuntimeException($package_path." not found");
 			$download_path = File::path(App::work("pear"),strtr($domain,".","_")."_".$target_package."_".strtr($target_version,".","_"));
@@ -92,11 +96,30 @@ class Pea{
 			if(!File::exist($download_path)){
 				File::untgz($download_url,$download_path);
 			}
+			self::$INSTALL[strtolower($domain."/".$package_name)] = $download_path;
 			//TODO 依存関係
-			$package_xml = self::$CHANNEL[$domain]."/r/package.".$target_version.".xml";
-//			if(Tag::setof($dependencies,R(Http)->do_get($package_xml)->body(),"dependencies")){
-//				
-//			}
+			$package_xml = File::exist(File::path($download_path,"package2.xml")) ? File::path($download_path,"package2.xml") : File::path($download_path,"package.xml");
+			if(Tag::setof($package,File::read($package_xml),"package")){
+				switch($package->inParam("version")){
+					case "1.0":
+						foreach($package->f("deps.in(dep)") as $dep){
+							if($dep->inParam("type")=="pkg"){
+								if(self::$OPTIONAL || $dep->inParam("optional") == "no") self::install($dep->value(),true);
+							}
+						}
+						break;
+					case "2.0":
+						foreach($package->f("dependencies.in(package)") as $dep){
+							$channel = $dep->f("channel.value()");
+							$name = $dep->f("name.value()");
+//							$optional = $dep->
+							//TODO option
+						}
+						break;
+					default:
+						throw new Exception("unknown package version");
+				}
+			}
 			
 			//TODO ファイルのコピー
 			
