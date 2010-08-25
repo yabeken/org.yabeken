aee7471c1974f894a175c1870647c0db127498fe
yabeken <yabeken@gmail.com>
2010-08-25 16:59:57 +0900

    bitstream 途中

--- libs/org/yabeken/io/stream/BitStream.php
+++ libs/org/yabeken/io/stream/BitStream.php
@@ -5,23 +5,79 @@ import('org.yabeken.io.stream.Stream');
  * @author yabeken
  * @license New BSD License
  */
-class BitIO extends Object{
-	protected $_resource_;
-	protected $_mode_;
+class BitStream extends Stream{
 	protected $_buf_;
-	protected $_pos_ = 0;
-	const MODE_READ = 1;
-	const MODE_WRITE = 2;
+	/**
+	 * Constructor
+	 * @param Stream $resource
+	 */
 	final protected function __new__(Stream $resource){
 		$this->_resource_ = $resource;
 	}
+	/**
+	 * Destructor
+	 */
 	final protected function __del__(){
-		if(!empty($this->_buf_) && $this->_resource_->is_closed()) throw new StreamException('stream is already closed');
-		//TODO
+		$this->close();
+	}
+	/**
+	 * get offset
+	 * @return array(byte-offset, bit-offset)
+	 */
+	protected function __get_offset__(){
+		return array($this->_resource_->offset(),$this->offset);
+	}
+	/**
+	 * set offset
+	 * @param integer $byte
+	 * @param integer $bit
+	 * @throws StreamException
+	 */
+	protected function __set_offset__($byte,$bit=0){
+		if($bit < 0 || $bit > 8) throw new StreamException('invalid bit offset {1}',$bit);
+		$this->_resource_->offset($byte);
+		$this->offset = $bit;
+	}
+	protected function __get_length__(){
+		return $this->_resource_->length();
+	}
+	public function open($resource,$mode=null){
+		return $this->_resource_->open($resource,$mode);
+	}
+	public public function close(){
+		return $this->_resource_->close();
+	}
+	public function seek($len,$mode=null){
+		$this->offset = 0;
+		$this->_buf_ = null;
+		return $this->_resource_->seek($len,$mode);
+	}
+	public function read($len=2048){
+		$this->offset = 0;
+		$this->_buf_ = null;
+		return $this->_resource_->read($len);
+	}
+	public function read_line($strict=false){
+		throw new StreamException('not implemented');
+	}
+	public function search($needle,$invert=false,$limit=null){
+		throw new StreamException('not implemented');
+	}
+	public function eof(){
+		return $this->_resource_->eof();
+	}
+	public function write($str){
+		return $this->_resource_->write($str);
+	}
+	public function is_opened(){
+		return $this->_resource_->is_opened();
+	}
+	public function is_closed(){
+		return $this->_resource_->is_closed();
 	}
 	/**
 	 * 1 bit
-	 * @return bit
+	 * @return binary
 	 */
 	public function get_bit(){
 		return $this->get_bits(1);
@@ -30,29 +86,15 @@ class BitIO extends Object{
 	 * read bits
 	 * @param integer $length
 	 * @return binary
+	 * @throws InvalidArgumentException
 	 */
 	public function get_bits($length){
-		$this->_mode_ == self::MODE_READ;
-		if(!is_int($length) || $length < 0) throw new RuntimeException("read_bits: invalid argument");
+		if(!is_int($length) || $length < 0) throw new InvalidArgumentException('invalid argument');
 		if($this->_buf_ === null){
-			$this->_buf_ = $this->_resource_read(1);
-			$this->_pos_ = 0;
+			$this->_buf_ = $this->_resource_->read(1);
 		}
 		$r = null;
 		$mask = ((0xFF << $this->_pos_) & 0xFF) >> $this->_pos_;
-		if($this->_pos_ + $num <= 8){
-			$r = ($this->_buf & $mask) >> $num;
-			$this->_pos_ += $num;
-		}else{
-			$r = ($this->_buf & $mask) << ($num - $this->_pos_);
-			$this->_buf_ = null;
-			$this->_pos_ = 0;
-			$r &= $this->get_bits($num - 8);
-		}
-		if($this->_pos_ == 8){
-			$this->_buf_ = null;
-			$this->_pos_ = 0;
-		}
 		return $r;
 	}
 	/**
@@ -68,8 +110,6 @@ class BitIO extends Object{
 	 * @param binary $bits
 	 */
 	public function put_bits($num,$bits){
-		if($this->_mode_ == self::MODE_READ) throw new RuntimeException("TODO");
-		$this->_mode_ == self::MODE_WRITE;
 		if($this->_pos_ + $num <= 8){
 //			$mask = 
 		}else{
